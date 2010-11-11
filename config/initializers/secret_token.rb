1607040a4a51b7e1e0ebe3acc886038afef33e62
Scott Croneberger <cppnerd@yahoo.com>
2010-11-11 21:05:17 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasker::Application.config.secret_token = '1e9aacb55cb972fe392902177fb768e6bc4326ea43113599b95ed83f144525cc146f95312b4151b02eb4c6f1bd58c115d2206228419b17a9f623c9f91d83bece'
